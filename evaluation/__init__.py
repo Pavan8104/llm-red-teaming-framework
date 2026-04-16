58ea7d1c8dd7418f8c7608962fd1a250b9b49be5
Pavan8104 <ps3297169@gmail.com>
2026-04-16 07:21:26 +0530

    fix evaluation __init__: align exports with actual function vs class signatures

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,3 +1,11 @@
 from .safety_scorer import SafetyScorer
-from .alignment_scorer import AlignmentScorer
+from .scorer import score_response, score_batch, score_summary
+from .alignment_scorer import score_alignment, score_alignment_batch
 from .metrics import compute_metrics
+
+__all__ = [
+    "SafetyScorer",
+    "score_response", "score_batch", "score_summary",
+    "score_alignment", "score_alignment_batch",
+    "compute_metrics",
+]
