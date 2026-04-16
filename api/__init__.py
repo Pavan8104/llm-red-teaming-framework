4821b300e5cb4e3200b4080c0963d54452d49bdb
Pavan8104 <ps3297169@gmail.com>
2026-04-16 07:21:26 +0530

    fix api __init__: export functions not class since llm_client is function-based

--- api/__init__.py
+++ api/__init__.py
@@ -1,13 +1,11 @@
-from .llm_client import LLMClient
+from .llm_client import send_prompt, ping
 from .mock_client import MockLLMClient
 from .providers import OpenAIProvider, AnthropicProvider, build_provider
 from .rate_limiter import AsyncRateLimiter
 
 __all__ = [
-    "LLMClient",
+    "send_prompt", "ping",
     "MockLLMClient",
-    "OpenAIProvider",
-    "AnthropicProvider",
-    "build_provider",
+    "OpenAIProvider", "AnthropicProvider", "build_provider",
     "AsyncRateLimiter",
 ]
